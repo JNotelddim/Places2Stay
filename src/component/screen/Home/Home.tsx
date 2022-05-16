52051a16da3ce1202402c84ab748a5af16e4fb69
JNotelddim <JNotelddim@users.noreply.github.com>
2022-05-16 15:45:18 -0700

    Add input to home screen

--- src/component/screen/Home/Home.tsx
+++ src/component/screen/Home/Home.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {SafeAreaView, StyleSheet} from 'react-native';
+import {SafeAreaView, StyleSheet, TextInput, View} from 'react-native';
 
 import {PlaceCTA, SectionHeader} from 'component/partial';
 
@@ -9,10 +9,19 @@ import img from './asset/stock-photo.jpg';
  * Home is the screen the user comes to first when they open the application
  */
 const Home: React.FC = () => {
-  // console.log('loading Home; isHermesInUse?', !!global.HermesInternal);
+  const [searchVal, setSearchVal] = React.useState('');
 
   return (
     <SafeAreaView style={styles.wrapper}>
+      <View style={styles.inputWrapper}>
+        <TextInput
+          style={styles.input}
+          placeholder="Try \'Boston\'"
+          value={searchVal}
+          onChangeText={setSearchVal}
+        />
+      </View>
+
       <SectionHeader
         heading="Hello MetaLab!"
         description="This is the description that goes on the home screen."
@@ -43,6 +52,26 @@ const styles = StyleSheet.create({
     paddingHorizontal: 50,
     backgroundColor: '#FFF1D2',
     height: '100%', // why doesn't flex: 1  have the same effect?
+  },
+  inputWrapper: {
+    backgroundColor: 'white',
+    paddingHorizontal: 6,
+    paddingVertical: 18,
+    marginBottom: 18,
+
+    border: '1px solid rgba(0, 0, 0, 0.19)',
+    borderRadius: 100,
+    shadowColor: '#000000',
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+    shadowOpacity: 0.17,
+    shadowRadius: 3.05,
+    elevation: 4,
+  },
+  input: {
+    textAlign: 'center',
   },
   cta: {
     marginTop: 15,
