847f02b808dbaa52d072efda38ede5e887ff6bf4
JNotelddim <JNotelddim@users.noreply.github.com>
2022-05-16 11:46:51 -0700

    Add TODO for adding variant prop to Text component later

--- src/component/base/Text/Text.tsx
+++ src/component/base/Text/Text.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import {Text as RNText, TextProps as RNTextProps} from 'react-native';
 
-interface TextProps extends RNTextProps {}
+interface TextProps extends RNTextProps {
+  // TODO: support different typography
+  variant?: string;
+}
 
 /**
  * Text is a wrapper for the RN Text component which allows for some customization.
  */
-export const Text: React.FC<TextProps> = props => {
-  return <RNText {...props} />;
+export const Text: React.FC<TextProps> = ({style, children}) => {
+  return <RNText style={style}>{children}</RNText>;
 };
 
 export default Text;
