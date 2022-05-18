26cc572c53784ebe4f10f9d2e5b0e8db378a8396
JNotelddim <JNotelddim@users.noreply.github.com>
2022-05-18 10:05:44 -0700

    Touch up modal card stylings

--- src/component/base/Modal/Modal.tsx
+++ src/component/base/Modal/Modal.tsx
@@ -17,22 +17,46 @@ const Modal: React.FC<ModalProps> = () => {
   return (
     <RNModal
       animationType="slide"
-      transparent={false}
+      transparent={true}
       visible={isModalOpen}
       onRequestClose={closeModal}>
-      <View style={styles.container}>
-        <Button title="close" onPress={closeModal} />
-        <Text>Modal</Text>
+      <View style={styles.overlay}>
+        <View style={styles.modalCard}>
+          {/* TODO: iconbutton */}
+          <Button title="close" onPress={closeModal} />
+
+          <Text>Modal</Text>
+        </View>
       </View>
     </RNModal>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
+  overlay: {
     height: Dimensions.get('screen').height,
     width: Dimensions.get('screen').width,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#ffffff77',
+  },
+  modalCard: {
+    marginTop: '5%',
+    height: '85%',
+    width: '95%',
+    borderRadius: 8,
     padding: 40,
+    backgroundColor: 'white',
+    opacity: 1,
+
+    shadowColor: '#000000',
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+    shadowOpacity: 0.17,
+    shadowRadius: 3.05,
+    elevation: 4,
   },
 });
 
