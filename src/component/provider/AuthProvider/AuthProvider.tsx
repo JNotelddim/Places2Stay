ec1d97f664b7f9244b80b4bd37a9522844a6a7d7
JNotelddim <JNotelddim@users.noreply.github.com>
2022-05-27 12:52:15 -0700

    Revert "[wip] try AuthSession's base useAuthRequest()"
    This reverts commit 777859cd244ab63a2294292ff8a6100b0d30240f.

--- src/component/provider/AuthProvider/AuthProvider.tsx
+++ src/component/provider/AuthProvider/AuthProvider.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
 
-import * as WebBrowser from 'expo-web-browser';
-import {makeRedirectUri, useAuthRequest, ResponseType} from 'expo-auth-session';
-// import {useIdTokenAuthRequest} from 'expo-auth-session/providers/google';
-
-WebBrowser.maybeCompleteAuthSession();
+// import { makeRedirectUri } from 'expo-auth-session';
+import {useIdTokenAuthRequest} from 'expo-auth-session/providers/google';
 
 const AuthContext = React.createContext({
   isAuthenticated: false,
@@ -13,70 +10,37 @@ const AuthContext = React.createContext({
 });
 
 // TODO: store for next session in storage
-const discovery = {
-  authorizationEndpoint: 'https://accounts.google.com/o/oauth2/v2/auth', // 'https://accounts.spotify.com/authorize',
-};
-
 export const AuthProvider: React.FC = ({children}) => {
   const [token, setToken] = React.useState<string | undefined>(undefined);
-  const [request, response, promptAsync] = useAuthRequest(
-    {
-      responseType: ResponseType.Token,
-      clientId: 'CLIENT_ID',
-      scopes: ['profile', 'email'],
-      redirectUri: makeRedirectUri({
-        scheme: 'org.reactjs.native.example.places2stay',
-      }),
-      codeChallenge: undefined,
-      codeChallengeMethod: undefined,
-    },
-    discovery,
-  );
-  // const [request, response, promptAsync] = useAuthRequest(
-  //   {
-  //     responseType: ResponseType.Token, // IdToken,
-  //     clientId: 'CLIENT_ID',
-  //     // 'xx-xx.apps.googleusercontent.com',
-  //     scopes: ['profile', 'email'], // ['userinfo.email'],
-  //     redirectUri: makeRedirectUri({
-  //       scheme: 'org.reactjs.native.example.places2stay',
-  //     }),
-  //   },
-  //   {
-  //     authorizationEndpoint: 'https://accounts.google.com/o/oauth2/v2/auth',
-  //   },
-  // );
-
+  const [, , promptAsync] = useIdTokenAuthRequest({
+    // ...Constants.manifest?.extra?.google,
+    iosClientId:
+      // TODO: move to manifest
+      '',
+    redirectUri: 'places2stay://',
+    // redirectUri: 'https://places2stay.com',
+    //redirectUri: 'org.reactjs.native.example.Places2Stay',
+  });
   const isAuthenticated = !!token;
 
   console.log({isAuthenticated, token});
 
-  React.useEffect(() => {
-    console.log({response});
-    if (response && response.type === 'success') {
-      const tokenResult = response.params.access_token;
-      console.log({tokenResult});
-      setToken(tokenResult);
-    }
-  }, [response]);
-
   const login = async () => {
     console.log('loginAttempt');
-    if (request) {
-      await promptAsync().catch(reason =>
-        console.log('[LoginPromptError] reason:', reason),
+
+    const response = await promptAsync().catch(reason =>
+      console.log('[LoginPromptError] reason:', reason),
+    );
+
+    if (response?.type !== 'success') {
+      throw new Error(
+        '[LoginPromptResponseError] Non-SuccessType: ' + response?.type,
       );
     }
-
-    // if (response?.type !== 'success') {
-    //   throw new Error(
-    //     '[LoginPromptResponseError] Non-SuccessType: ' + response?.type,
-    //   );
-    // }
-    // console.log('setToken', response.params.id_token, {
-    //   params: response.params,
-    // });
-    // setToken(response.params.id_token);
+    console.log('setToken', response.params.id_token, {
+      params: response.params,
+    });
+    setToken(response.params.id_token);
   };
 
   const logout = () => {
