131ef1bf8e1111214cb95036ddbb4a9b82a1e8fb
JNotelddim <JNotelddim@users.noreply.github.com>
2022-05-27 13:26:47 -0700

    Add logout button in Account

--- src/component/screen/Account/Account.tsx
+++ src/component/screen/Account/Account.tsx
@@ -1,17 +1,25 @@
-import {Text} from 'component/base';
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import {Button, StyleSheet, View} from 'react-native';
+
+import {Text} from 'component/base';
+import {useAuth} from 'component/provider';
+import {spacing} from 'const';
 
 const Account: React.FC = () => {
+  const {logout} = useAuth();
   return (
     <View style={styles.container}>
       <Text>Account</Text>
+      <Button title="log out" onPress={logout} />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    paddingTop: spacing.whitespace.unsafeScreenTop,
+    paddingHorizontal: spacing.whitespace.large,
+  },
 });
 
 export default Account;
